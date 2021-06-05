c537910d48fd9180f13630856db1b714c80fbe10
Radika <radikakumar@gmail.com>
2021-06-05 23:24:40 +0100

    transferred dictionary code from previous file.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-
 import './App.css';
-import Weather from "./Weather"
+import Dictionary from "./Dictionary";
 export default function App() {
   return (
     <div className="App">
-      
-        <div>
-          
-          <Weather />
-        </div>
-        
-     
+      <header className="App-header">
+        <div className="Container">
+          </div>
+      <div className= "DictionaryApp">
+     📔 Dictionary App
+      </div>
+      <main>
+        <Dictionary />
+      </main>
+    
+      </header>
     </div>
+  
   );
 }
 

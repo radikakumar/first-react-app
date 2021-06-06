4db33074aa8cedf6001cdcab43a7fcaac865bfe1
Radika <radikakumar@gmail.com>
2021-06-06 15:46:34 +0100

    Update Synonyms.js

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,18 +3,16 @@ import React from "react";
 export default function Synonyms(props) {
     if (props.synonyms) {
     return (
+        <div>
         <ul className="Synonyms">
-            {props.synonyms.map(function (synonyms , index){
+            {props.synonyms.map(function (synonyms,index){
         <li key={index}>
             {synonyms}
         </li>
-            }
-            
-            
-            
-            )}
+            })}
         </ul>
-    )
+        </div>
+    );
     } else {
         return null;
     }

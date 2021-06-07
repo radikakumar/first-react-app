1de55ba8d5be3580945c53e3647e9570fd743154
Radika <radikakumar@gmail.com>
2021-06-07 11:28:29 +0100

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -1,34 +1,29 @@
 import React from "react";
-import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
-export default function Results (props){
-console.log (props.content)
-if (props.content) {
+import Meaning from "./Meaning";
+
+export default function Results(props) {
+  if (props.content) {
     return (
-        <div>
-    <h2> {props.content.word}</h2>
-    {props.content.phonetics.map(function (phonetic, index){
-return(
-    <div key={index}>
-        <Phonetic phonetic={phonetic} />
-        </div>
-)
-
-
-    })}
-    {props.content.meanings.map(function(meaning,index){
-     return (
-        <div key={index}>
-        <Meaning meaning={meaning} />
-        </div>
-        )
-
-
-    } )}
-        </div>
-    )
-} else {
+      <div className="Results">
+        <h2>{props.content.word}</h2>
+        {props.content.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
+        {props.content.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
     return null;
-}
-
+  }
 }
